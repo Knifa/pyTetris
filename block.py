2a71297731015dd511f9e617e927cf977cfb2ce1
Tom Whiteley <xzanaku@gmail.com>
2011-10-22 21:27:46 +0100

    block.py, block.png

--- block.py
+++ block.py
@@ -1,11 +1,20 @@
 import pygame
 
 class Block(object):
-	def __init__(self):
-		pass
+        def __init__(self):
+            #state is a boolean, where true = falling, false = stationary
+            self.state = True
+            #loads the image block.png from the root dir and converts it
+            #for use with the current screen.
+            self.image = pygame.image.load('/home/tom/Documents/Python/VIDEOGAMES/pyTetris/block.png').convert()
 		
-	def draw(self):
-		pass
-		
-	def update(self):
-		pass
+        #taking parameters of self, x co-ord, y co-ord, and display screen.
+        def draw(self, x, y, screen):
+                #draws the block at location (x,y) on display screen.
+                screen.blit(self.image, (x,y))
+                
+        def update(self):
+                #use for animation later possibly?
+                pass
+
+
