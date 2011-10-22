7e08740c57688d2476431dae4ec330ef4b2e71cb
Daniel Callander <knifacat@gmaiil.com>
2011-10-22 21:26:18 +0100

    added support for menu in stuff and game states.

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import pygame
 import pygame.locals as local
 
 from grid import *
+from menu import *
 
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
+
+STATE_MENU = 0
+STATE_GAME = 1
 
 class Game(object):
     def __init__(self):
@@ -16,12 +20,20 @@ class Game(object):
             
         # Game is running! Or not.
         self.is_running = False
+        
+        # Current game state.
+        # self.state = STATE_MENU
+        self.state = STATE_GAME
+        
+        # key presses for the current frame
+        self.key_presses = {}
 		
     def run(self):
 	    # The game should be running now!
         self.is_running = True
 		
-		# Create a new game grid.
+		# Create our main game objects.
+        self.menu = Menu()
         self.grid = Grid()
 		
 		# Loop while the game is running, obviously
@@ -31,13 +43,25 @@ class Game(object):
                 if event.type == local.QUIT:
 					# Window close event.
                     self.is_running = False
+                elif event.type == local.KEYDOWN:
+                    self.key_presses[event.key] = True
+                elif event.type == local.KEYUP:
+                    del self.key_presses[event.key]
 			
-			# Update and draw the grid.
-            self.grid.update(self)
-            self.grid.draw()
+			# Update and draw the right thing depedning on the
+			# game state!
+            if self.state == STATE_MENU:
+			    self.menu.update(self)
+			    self.menu.draw()
+            elif self.state == STATE_GAME:
+                self.grid.update(self)
+                self.grid.draw()
 					
             # Flip the display.
             pygame.display.update()
+            
+            # Reset the key presses
+            print self.key_presses
 	
 game = Game()
 game.run()
