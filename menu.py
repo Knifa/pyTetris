02c32cebd76c6468940b0fc560a7bba2fcdccb19
Sean Galbraith <seang021@gmail.com>
2011-10-22 23:30:28 +0100

    Menu now properly allows access to game.

--- menu.py
+++ menu.py
@@ -7,8 +7,13 @@ class Menu(object):
     	pass
 
     def update(self,game):
-	if local.K_COMMA in game.key_presses:
-		print "Yes!"
+	if local.K_UP in game.key_presses:
+		print "Up"
+	elif local.K_DOWN in game.key_presses:
+		print "Down"
+	elif local.K_x in game.key_presses:
+		game.state = 1
 
     def draw(self):
 	surf = pygame.display.get_surface()
+	
