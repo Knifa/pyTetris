092063b571bd0f410c866f1b8a17ba698c9d5bac
Daniel Callander <knifacat@gmaiil.com>
2011-10-22 20:49:44 +0100

    some update

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
 import pygame
+import pygame.locals as local
+
+import grid
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
 
 class Game(object):
 	def __init__(self):
-		pass
+		pygame.init()
+		
+		# Create our display.
+		self.display = pygame.display.set_mode(
+			(SCREEN_WIDTH, SCREEN_HEIGHT))
+		
+		# Game is running! Or not.
+		self.is_running = False
 		
 	def run(self):
-		pass
+	    # The game should be running now!
+        self.is_running = True
 		
-if __file__ == "__main__":
-	game = Game()
-	game.run()
+		# Create a new game grid.
+		self.grid = Grid()
+		
+		# Loop while the game is running, obviously
+		while self.is_running:
+			# Read in SDL events.
+			for event in pygame.event.get():
+				if event.type == local.QUIT:
+					# Window close event.
+					self.is_running = False
+			
+			# Update and draw the grid.
+			grid.update(self)
+			grid.draw()
+					
+			# Flip the display.
+			pygame.display.update()
+	
+game = Game()
+game.run()
